d3bdd4fb8c10990060a85106c894de3602d8b989
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2022-12-06 17:37:52 +0000

    text area

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [text, setText] = React.useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center px-4">
+      <h1 className="p-4">Markdown Previewer</h1>
+      <textarea
+        value={text}
+        name="text"
+        id="text"
+        rows="10"
+        className="textarea"
+      ></textarea>
     </div>
   );
 }
